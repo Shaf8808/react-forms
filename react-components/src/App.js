e90b5ea7bd8dd8c180b215c24b6c55b61c576b08
Shaf8808 <shadmankhan08@gmail.com>
2023-09-15 13:58:19 +0000

    Add hooked components

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import ControlledForm from './components/ControlledForm';
 import UncontrolledForm from './components/UncontrolledForm';
+import ControlledFormHooks from './components/ControlledFormHooks';
+import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 function App() {
   return (
     <div className="App">
       {/* <ControlledForm /> */}
-      <UncontrolledForm />
+      {/* <UncontrolledForm /> */}
+      {/* <ControlledFormHooks /> */}
+      {/* <UseStateWithArrays /> */}
+      <UseStateWithObjects />
     </div>
   );
 }
